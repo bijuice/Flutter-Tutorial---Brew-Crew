cfc1d423ccf5ad5b100c4d6c3d77b4bee69e0e13
bijuice <abdul.r.zaid@gmail.com>
2020-12-21 13:10:02 +0300

    lesson 14

--- lib/screens/authenticate/register.dart
+++ lib/screens/authenticate/register.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:brew_crew/services/auth.dart';
+import 'package:brew_crew/shared/constants.dart';
 
 class Register extends StatefulWidget {
   final Function toggleView;
@@ -43,6 +44,7 @@ class _RegisterState extends State<Register> {
               children: <Widget>[
                 SizedBox(height: 20.0),
                 TextFormField(
+                  decoration: textInputDecoration.copyWith(hintText: 'Email'),
                   validator: (val) => val.isEmpty ? 'Enter an email' : null,
                   onChanged: (val) {
                     setState(() => email = val);
@@ -50,6 +52,8 @@ class _RegisterState extends State<Register> {
                 ),
                 SizedBox(height: 20.0),
                 TextFormField(
+                  decoration:
+                      textInputDecoration.copyWith(hintText: 'Password'),
                   validator: (val) =>
                       val.length < 6 ? 'Enter a password 6+ chars long' : null,
                   obscureText: true,
