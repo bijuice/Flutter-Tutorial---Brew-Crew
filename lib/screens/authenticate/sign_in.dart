a81a8eb0629dcbbf16be4c67541c2937fcae5441
bijuice <abdul.r.zaid@gmail.com>
2020-12-21 12:48:22 +0300

    lesson 12

--- lib/screens/authenticate/sign_in.dart
+++ lib/screens/authenticate/sign_in.dart
@@ -12,10 +12,12 @@ class SignIn extends StatefulWidget {
 
 class _SignInState extends State<SignIn> {
   final AuthService _auth = AuthService();
+  final _formKey = GlobalKey<FormState>();
 
   //text field state
   String email = '';
   String password = '';
+  String error = '';
 
   @override
   Widget build(BuildContext context) {
@@ -37,16 +39,20 @@ class _SignInState extends State<SignIn> {
         body: Container(
           padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 50.0),
           child: Form(
+            key: _formKey,
             child: Column(
               children: <Widget>[
                 SizedBox(height: 20.0),
                 TextFormField(
+                  validator: (val) => val.isEmpty ? 'Enter an email' : null,
                   onChanged: (val) {
                     setState(() => email = val);
                   },
                 ),
                 SizedBox(height: 20.0),
                 TextFormField(
+                  validator: (val) =>
+                      val.length < 6 ? 'Enter a password 6+ chars long' : null,
                   obscureText: true,
                   onChanged: (val) {
                     setState(() => password = val);
@@ -57,15 +63,28 @@ class _SignInState extends State<SignIn> {
                 ),
                 RaisedButton(
                   onPressed: () async {
-                    print(email);
-                    print(password);
+                    if (_formKey.currentState.validate()) {
+                      dynamic result = await _auth.signInWithEmailAndPassword(
+                          email, password);
+
+                      if (result == null) {
+                        setState(() {
+                          error = 'Could not sign with those credentials';
+                        });
+                      }
+                    }
                   },
                   color: Colors.pink[400],
                   child: Text(
                     'Sign In',
                     style: TextStyle(color: Colors.white),
                   ),
-                )
+                ),
+                SizedBox(
+                  height: 20.0,
+                ),
+                Text(error,
+                    style: TextStyle(color: Colors.red, fontSize: 14.0)),
               ],
             ),
           ),
