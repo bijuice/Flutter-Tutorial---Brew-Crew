7eeba4b6a02152e218025bf8b90a135cbabd5b32
bijuice <abdul.r.zaid@gmail.com>
2020-12-24 18:30:46 +0300

    lesson 26

--- lib/screens/home/brew_list.dart
+++ lib/screens/home/brew_list.dart
@@ -1,4 +1,3 @@
-import 'package:brew_crew/shared/loading.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:brew_crew/models/brew.dart';
@@ -12,17 +11,13 @@ class BrewList extends StatefulWidget {
 class _BrewListState extends State<BrewList> {
   @override
   Widget build(BuildContext context) {
-    final brews = Provider.of<List<Brew>>(context);
+    final brews = Provider.of<List<Brew>>(context) ?? [];
 
-    if (brews != null) {
-      return ListView.builder(
-        itemCount: brews.length,
-        itemBuilder: (context, index) {
-          return BrewTile(brew: brews[index]);
-        },
-      );
-    } else {
-      return Loading();
-    }
+    return ListView.builder(
+      itemCount: brews.length,
+      itemBuilder: (context, index) {
+        return BrewTile(brew: brews[index]);
+      },
+    );
   }
 }
